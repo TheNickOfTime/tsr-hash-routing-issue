5397ae3a76e998402bf9ffd3d858727af1e1299b
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2025-04-14 21:03:00 +0000

    feat: configuring github pages

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ const router = createRouter({
 	scrollRestoration: true,
 	defaultStructuralSharing: true,
 	defaultPreloadStaleTime: 0,
+	basepath: '/tsr-hash-link-issue',
 });
 
 // Register the router instance for type safety
